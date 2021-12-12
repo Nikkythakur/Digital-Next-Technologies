ed40b58a0c656d8dc4c82cf4623a8a238de52efc
Nikita Thakur <77138407+Nikkythakur@users.noreply.github.com>
2021-12-12 15:11:42 +0530

    Add files via upload

---
+++ CA - Copy/footer.php
@@ -0,0 +1,115 @@
+	<!-- Footer -->
+    <footer class="site-footer style-3" id="footer" style="background-image: url(images/background/bg11.png), var(--gradient-sec); background-size: cover, 200%; ">
+		<div class="footer-top">
+            <div class="container">
+				<div class="row">
+					<div class="col-xl-4 col-lg-12 col-md-4 col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.2s">
+                        <div class="widget widget_about">
+							<div class="footer-logo">
+								<a href="index.html"><img src="images/logo-white.png" alt=""/></a> 
+							</div>
+							<p>Digital Next Technologies is a full-service digital marketing agency that helps businesses and brands to attract, impress and convert more leads online.</p>
+							<div class="dlab-social-icon">
+								<ul>
+									<li><a class="fa fa-facebook" href="https://www.facebook.com/digitalnexttechnologies" target="_blank"></a></li>
+									<li><a class="fa fa-instagram" href="https://www.instagram.com/digital_next_technology/" target="_blank"></a></li>
+									<li><a class="fa fa-linkedin" href="https://www.linkedin.com/company/digital-next-technologies" target="_blank"></a></li>
+								</ul>
+							</div>
+						</div>
+                    </div>
+					<div class="col-xl-2 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.4s">
+						<div class="widget widget_services style-1">
+							<h5 class="footer-title">Our links</h5>
+							<ul>
+								<li><a href="https://digitalnextworld.com/">Home</a></li>
+								<li><a href="about-us.php">About Us</a></li>
+								<li><a href="smo-and-smm.php">SMO & SMM</a></li>
+								<li><a href="contact-us.php">Contact Us</a></li>
+								                     <li class="paypay">
+                     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick">
+                        <input type="hidden" name="hosted_button_id" value="JQM5BLHDS3WVC">
+                        <input type="image" src="images/paypal.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                     <!-- <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"> -->
+                      </form>
+                  </li>								
+							</ul>
+						</div>
+                    </div>
+					<div class="col-xl-3 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.6s">
+						<div class="widget widget_services style-1">
+							<h5 class="footer-title">Our Services</h5>
+							<ul>
+								<li><a href="web-designing.php">Web Designing</a></li>
+								<li><a href="web-development.php">Web Development</a></li>
+								<li><a href="static-website-designing.php">Static Website</a></li>
+								<li><a href="corporate-website-designing.php">Corporate Website</a></li>
+								<li><a href="cms-base-website.php">CMS Base Website</a></li>
+							</ul>
+						</div>
+                    </div>
+				<!-- 	<div class="col-xl-2 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="0.8s">
+						<div class="widget widget_services style-1">
+						   <h5 class="footer-title">Other links</h5>
+							<ul>
+								<li><a href="javascript:void(0);">FAQ</a></li>
+								<li><a href="javascript:void(0);">Portfolio</a></li>
+								<li><a href="privacy-and-policy.php">Privacy Policy</a></li>
+								<li><a href="javascript:void(0);">Terms & Conditions</a></li>
+								<li><a href="cancellation.php">Cancellation</a></li>
+							</ul>
+						</div>
+                    </div> -->
+					<div class="col-xl-3 col-lg-3 col-md-4  col-sm-6 wow fadeIn" data-wow-duration="2s" data-wow-delay="1.0s">
+						<div class="widget widget_getintuch">
+							<h5 class="footer-title">Contact Us</h5>
+							<ul>
+								<li>
+									<i class="fa fa-phone gradient"></i>
+									<span>+971-586353516</span> 
+								</li>
+								<li>
+									<i class="fa fa-envelope gradient"></i> 
+									<span>info@digitalnextworld.com</span>
+								</li>
+								<li>
+									<i class="fa fa-map-marker gradient"></i>
+									<span>Digital Next Technologies, Dubai (United Arab Emirates)</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+                </div>
+            </div>
+		</div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-12 text-center"> 
+						<span class="copyright-text">© Copyright 2021. All Rights Reserved |<a href="https://www.digitalnextworld.com/" target="_blank" style="color:#fff;"> Digital Next technologies</a></span> 
+					</div>
+                </div>
+            </div>
+        </div>
+    </footer>
+	<button class="scroltop icon-up" type="button"><i class="fa fa-arrow-up"></i></button>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="vendor/wow/wow.js"></script>
+<script src="vendor/bootstrap/js/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/owl-carousel/owl.carousel.js"></script>
+<script src="vendor/magnific-popup/magnific-popup.js"></script>
+<script src="vendor/counter/waypoints-min.js"></script>
+<script src="vendor/counter/counterup.min.js"></script>
+<script src="vendor/imagesloaded/imagesloaded.js"></script>
+<script src="vendor/masonry/masonry-3.1.4.js"></script>
+<script src="vendor/masonry/masonry.filter.js"></script>
+<script src="vendor/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="js/dz.carousel.js"></script>
+<script src="js/dz.ajax.js"></script>
+<script src="js/custom.js"></script>
+<script src="vendor/rangeslider/rangeslider.js"></script>
+</body>
+</html>
